5a8092a567ec8d56fee31462a6c8e19cadbcbbd2
bseony2 <bseony2@gmail.com>
2022-10-10 17:55:21 +0900

    피보나치 수 - 프로그래머스 lv.2 - 구현, 피보나치 수열
    https://school.programmers.co.kr/learn/courses/30/lessons/12945

--- 배선영/피보나치 수.java
+++ 배선영/피보나치 수.java
@@ -6,11 +6,12 @@ class Solution {
         
         for(int i= 3; i<n + 1; i++) {
             answer = (pre + post) % 1234567;
+            //n에 대한 피보나치 수만 구하면 되기 때문에 n 이전의 수자는 메모리에 할당하지 않음
             pre = post;
             post = answer;
         }
         
         return answer;
     }
-    
+
 }
