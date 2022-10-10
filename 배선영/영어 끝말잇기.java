efba48b00ed999b95af683455636fa24c01ac3a6
bseony2 <50575427+bseony2@users.noreply.github.com>
2022-10-10 22:57:59 +0900

    영어 끝말잇기 - 프로그래머스 lv.2 - 구현
    https://school.programmers.co.kr/learn/courses/30/lessons/12981

---
+++ 배선영/영어 끝말잇기.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+        
+        int turn = 0;
+
+        List<String> wordList = new ArrayList<String>(n);
+        wordList.add(words[0]);
+        for(int i=1; i<words.length; i++) {
+            String preWord = words[i - 1];
+            String currentWord = words[i];
+            if(  words[i - 1].charAt(preWord.length() - 1) != currentWord.charAt(0) // 이전 단어에 정상적인 끝말잇기가 아닌 경우
+                || wordList.contains(currentWord) ) { // 이미 말한 단어를 말한 경우
+                turn = i;
+                break;
+            }
+            wordList.add(currentWord);
+        }
+        return turn == 0 ? new int[] {0, 0} : new int[] {(turn % n) + 1, turn/n + 1};
+
+    }
+}
