4933fd8f66466d31c319524e3c1fb619889e773a
yachoo0611 <dpcksdl3@naver.com>
2022-11-03 00:15:12 +0900

    토마토 - 백준 - BFS

--- 김예찬/BJ-7576.java
+++ 김예찬/BJ-7576.java
@@ -13,7 +13,7 @@ public class Boj_7576_new {
 	static int arr[][];
 	static Queue<DO> queue = new LinkedList<>();
 	static int dx[] = { 1, -1, 0, 0 };
-	static int dy[] = { 0, 0, 1, -1 };
+	static int dy[] = { 0, 0, 1, -1 }; 
 
 	public static void main(String args[]) throws Exception {
 
