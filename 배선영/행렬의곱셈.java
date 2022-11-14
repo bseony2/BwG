dbd6f77ba598ce77db474b40ac1eb906d71638b9
bseony2 <bseony2@gmail.com>
2022-11-14 20:49:09 +0900

    행렬의 곱셈 - 백준 골드 4 - 분할 정복 https://www.acmicpc.net/problem/10830

--- 배선영/행렬의곱셈.java
+++ 배선영/행렬의곱셈.java
@@ -3,13 +3,14 @@ import java.lang.StringBuilder;
 
 public class 행렬의곱셈 {
     static int N;
+    static int[][] basicMtx;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb;
         int[][] answerMtx = new int[N][N];
         N = sc.nextInt();
 		long B = sc.nextLong();	// 타입 주의
-        int[][] basicMtx = new int[N][N];
+        basicMtx = new int[N][N];
 
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
@@ -17,9 +18,7 @@ public class 행렬의곱셈 {
             }
         }
 
-        for(int i=0; i<B-1; i++) {
-            answerMtx = i == 0 ? multiplyMatrix(basicMtx, basicMtx) : multiplyMatrix(answerMtx, basicMtx);
-        }
+        answerMtx = devide(basicMtx, B);
 
         for(int i=0; i<N; i++) {
             sb = new StringBuilder();
@@ -30,6 +29,22 @@ public class 행렬의곱셈 {
         }
         sc.close();
 
+    }
+
+    static int[][] devide(int[][] matrix, long count) {
+        if(count == 1) {
+            return matrix;
+        }
+
+        int[][] middleMtx = devide(matrix, count / 2);
+
+        middleMtx = multiplyMatrix(middleMtx, middleMtx);
+
+        if(count % 2 == 1) {
+            middleMtx = multiplyMatrix(middleMtx, basicMtx);
+        }
+
+        return middleMtx;
     }
 
     static int[][] multiplyMatrix(int[][] a, int[][] b) {
