e36f8ea9cfbf8677832f1a49a3ed5f5a0548c314
bseony2 <bseony2@gmail.com>
2023-02-20 23:16:07 +0900

    백준 : 최소 스패닝 트리 - 최소 신장 트리

--- 배선영/최소스패닝트리.java
+++ 배선영/최소스패닝트리.java
@@ -8,25 +8,21 @@ import java.util.Arrays;
 public class 최소스패닝트리 {
     static int parent[];
 
-    static boolean isUnion(int x, int y) {
-        return x == parent[y] || y == parent[x] || (parent[y] != -1 && parent[y] == parent[x]);
-    }
-
     static int find(int x) {
         if(parent[x] < 0) {
             return x;
+        } else {
+            return parent[x] = find(parent[x]);
         }
-
-        return parent[x] = find(parent[x]);
     }
 
-    static void union(int inputX, int inputY) {
-        int x = find(inputX);
-        int y = find(inputY);
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
 
-        if(x==y) return;
-
-        if(x < y) {
+        if(x == y) {
+            return;
+        }else if(parent[x] < parent[y]) {
             parent[x] += parent[y];
             parent[y] = x;
         } else {
@@ -35,37 +31,41 @@ public class 최소스패닝트리 {
         }
     }
 
+    static boolean isUnion(int x, int y) {
+        return find(x) == find(y);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int V = 0;
-        int E = 0;
-        
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        ArrayList<int[]> pQ = new ArrayList<int[]>();
-        int answer = 0;
-
-        for(int i=0; i<E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-            int edge = Integer.parseInt(st.nextToken());
-
-            pQ.add(new int[] {v1, v2, edge});
-        }
-
-        pQ.sort((a, b) -> a[2] - b[2]);
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
         parent = new int[V + 1];
         Arrays.fill(parent, -1);
-        for(int i = 0 ; i<E; i++) {
-            int[] arr = pQ.get(i);
 
-            if(!isUnion(arr[0], arr[1])) {
-                union(arr[0], arr[1]);
-                answer += arr[2];
+        ArrayList<Link> linkList = new ArrayList<>();
+
+        for(int i=0; i<E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            linkList.add(new Link(start, dest, cost));
+        }
+
+        linkList.sort((a, b) -> a.cost - b.cost);
+
+        int answer = 0;
+
+        for(int i = 0 ; i<E; i++) {
+            Link link = linkList.get(i);
+
+            if(!isUnion(link.start, link.dest)) {
+                union(link.start, link.dest);
+                answer += link.cost;
             }
         }
 
@@ -73,8 +73,14 @@ public class 최소스패닝트리 {
     }
 }
 
+class Link {
+    int start;
+    int dest;
+    int cost;
 
-// 3 3
-// 1 2 1
-// 2 3 2
-// 1 3 3
+    public Link(int start, int dest, int cost) {
+        this.start = start;
+        this.dest = dest;
+        this.cost = cost;
+    }
+}
