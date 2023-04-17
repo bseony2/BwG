c08cf170012f316a9509b631e5eb5af29503f455
bseony2 <bseony2@gmail.com>
2023-04-17 23:14:56 +0900

    SWEA 6808 : 규영이와 인영이의 카드게임 - 백트래킹

--- 배선영/규영이와인영이의카드게임.java
+++ 배선영/규영이와인영이의카드게임.java
@@ -3,84 +3,79 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.lang.StringBuilder;
 import java.util.StringTokenizer;
 
 class 규영이와인영이의카드게임 {
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static int[] myCards;
-    static int win = 0;
-    static int lose = 0;
-    public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int win;
+    static int lose;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
+        
         for(int t=1; t<=T; t++) {
-            myCards = new int[9];
+            int[] myCards = new int[9];
             boolean[] selectedCard = new boolean[19];
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
             for(int i=0; i<9; i++) {
                 int value = Integer.parseInt(st.nextToken());
-                myCards[i] = value;
+                myCards[i] = value;         // 내 카드
                 selectedCard[value] = true;
             }
 
-            int[] otherCards = new int[9];
-            int index = 0;
-            for(int i=1; i<=18; i++) {
-                if(!selectedCard[i]) {
-                    otherCards[index++] = i;
-                }
+            int[] opponentCards = new int[9];
+            for(int i=1,index=0; i<19; i++) {
+                if(!selectedCard[i]) opponentCards[index++] = i;    // 상대방 카드
             }
 
-            win = 0;
-            lose = 0;
-            play(otherCards, new boolean[9], 0, 0, 0);
-            
-            System.out.println("#"+t + " " + win + " " + lose + "\n");
+            win = lose = 0;
+            play(myCards, opponentCards, new boolean[9], 0, 0, 0);
+            sb.append("#"+t).append(" " + win).append(" " + lose + "\n");
         }
 
+        bw.write(sb.toString());
+        bw.flush();
+
+        br.close();
+        bw.close();
     }
 
-    // 순열 작성해야 함
-    static void play(int[] otherCards, boolean[] isSelected, int depth, int myScore, int otherScore) {
-        
-        if(myScore > 85 || otherScore > 85) {
-            int extraScore = 1;
-            for(int i=9-depth; i>0; i--) {
-                extraScore *= i;
+    static void play(int[] myCards, int[] opponentCards, boolean[] isVisited, int depth, int myScore, int opponentScore) {
+        if(myScore > 85 || opponentScore > 85 || depth ==9) {    // 1부터 18까지 다 더해봐야 171임
+            int afterValue = 1;
+            for(int i = 9-depth; i>1; i--) {
+                afterValue *= i;    // 한쪽이 이미 85점을 넘는다면 그 아래 depth에 있는 모든 경우의 수의 승패도 똑같다 그러니까 팩토리얼로 그 아래 경우의 수를 구한다
             }
-
-            if(myScore > otherScore) win += extraScore;
-            else lose += extraScore;
+            if(myScore > 85) win += afterValue;
+            else if(opponentScore > 85) lose += afterValue;
 
             return;
         }
 
-        for(int i=0; i<9; i++) {
-            if(!isSelected[i]) {
-                
-                isSelected[i] = true;
+        for(int i=0; i<9; i++) {    // 상대방이 내는 카드의 순서가 유의미 하므로 조합이 아닌 순열로 짜야함
+            if(!isVisited[i]) {
+                isVisited[i] = true;
 
-                int sum = myCards[depth] + otherCards[i];
-                if(myCards[depth] > otherCards[i]) {
-                    myScore += sum;
-                }
-                else {
-                    otherScore += sum;
-                }
-                
-                play(otherCards, isSelected, depth+1, myScore, otherScore);
+                int score = myCards[depth] + opponentCards[i];  // 내 카드의 순서는 고정적이므로 depth 값을 사용해야 한다
 
-                if(myCards[depth] > otherCards[i]) {
-                    myScore -= sum;
-                }
-                else {
-                    otherScore -= sum;
+                if(myCards[depth] > opponentCards[i]) {
+                    myScore += score;
+                } else {
+                    opponentScore += score;
                 }
 
-                isSelected[i] = false;
+                play(myCards, opponentCards, isVisited, depth + 1, myScore, opponentScore);
+
+                if(myCards[depth] > opponentCards[i]) {
+                    myScore -= score;
+                } else {
+                    opponentScore -= score;
+                }
+                isVisited[i] = false;
             }
         }
     }
