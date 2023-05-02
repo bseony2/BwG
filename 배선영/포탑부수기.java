b1ee5b7868c060baccea76b5b304c001c0c26c9c
bseony2 <bseony2@gmail.com>
2023-05-02 22:15:49 +0900

    SWEA : 포탑 부수기 - 시뮬레이션, BFS

--- 배선영/포탑부수기.java
+++ 배선영/포탑부수기.java
@@ -4,17 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.lang.Comparable;
 
-public class Main {
+class Tower implements Comparable<Tower>{
+    int r, c, power, turn, backR, backC;
+
+    public Tower(int r, int c, int power) {
+        this.r = r;
+        this.c = c;
+        this.power = power;
+    }
+
+    public int compareTo(Tower tower) {
+        if(this.power != tower.power) return this.power - tower.power;
+        if(this.turn != tower.turn) return tower.turn - this.turn;
+        if(this.r+this.c != tower.r + tower.c) return (tower.r+tower.c) - (this.r+this.c);
+        return tower.c - this.c;
+    }
+}
+
+public class 포탑부수기 {
 
     static int N;
     static int M;
     static int K;
-    static int[][] field;
-    static int[][] attackTime;
-    static int[] dr = new int[]{0, 1, 0, -1, 1, -1, 1, -1};  //우, 하, 좌, 상, 우하, 우상, 좌하, 좌상
-    static int[] dc = new int[]{1, 0, -1, 0, 1, 1, -1, -1};
-    static LinkedList<int[]> subTarget;
+    static Tower[][] field;
+    static int[] dr = new int[]{0, 1, 0, -1, 1, 1, -1, -1};  //우, 하, 좌, 상, 우하, 좌하, 좌상, 우상
+    static int[] dc = new int[]{1, 0, -1, 0, 1, -1, -1, 1};
+    static Queue<Tower> queue;
+    static Queue<Tower> damagedTower;
+    static Tower attacker;
+    static Tower target;
+    static boolean[][] isVisited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -22,197 +43,170 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        field = new int[N][M];
-        attackTime = new int[N][M];
+        field = new Tower[N][M];
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for(int j=0; j<M; j++) {
-                field[i][j] = Integer.parseInt(st.nextToken());
+                int value = Integer.parseInt(st.nextToken());
+                if(value != 0) field[i][j] = new Tower(i, j, value);
             }
         }
 
-        play();
-
-        System.out.println(findTower());
-
+        for(int k=1; k<=K; k++) {
+            init();
+            selectTower(k);
+            
+            isVisited[attacker.r][attacker.c] = true;
+            
+            queue.offer(attacker);
+            boolean laser = laserPossible();
+            
+            target.power -= attacker.power;
+            if(laser) laserAttack();
+            else bombAttack();
+            destroyOrHeal();
+            if(isFinish()) break;
+        }
+        System.out.println(findStrongestTower());
     }
 
-    static void play() {
-        for(int k=0; k<K; k++) {
-            int[] attacker = findAttacker(k);
-            int r = attacker[0];
-            int c = attacker[1];
-            field[r][c] += N+M;
-            int power = field[r][c];
-            int[] target = findTarget(r, c);
-
-            if(N == target[0] && M == target[1]) return;
-
-            boolean[][] isVisited = new boolean[N][M];
-            isVisited[r][c] = true;
-            subTarget = new LinkedList<int[]>();
-
-            findLaserWay(r, c, target, isVisited, new LinkedList<int[]>(), 0);
-            field[target[0]][target[1]] -= power;   // 타겟은 일단 한방
-            if(subTarget.size() > 0) { // 레이저 공격 경로가 있을 경우
-                for(int[] current : subTarget) {
-                    field[current[0]][current[1]] -= power/2;
-                }
-            }
-            else {  // 포탄 공격
-                for(int i=0; i<8; i++) {
-                    int nr = target[0] + dr[i];
-                    int nc = target[1] + dc[i];
-                    if(nr < 0) nr = N-1;
-                    if(nr == N) nr = 0;
-                    if(nc < 0) nc = M-1;
-                    if(nc == M) nc = 0;
-
-                    if((nr == r && nc ==c) || field[nr][nc] <= 0) continue;
-
-                    subTarget.add(new int[]{nr, nc});
-                    field[nr][nc] -= power/2;
-                }
-            }
-
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<M; j++) {
-                    if(field[i][j] <= 0) continue;
-                    field[i][j] += 1;
-                }
-            }
-            field[r][c] -= 1;
-            field[target[0]][target[1]] -= 1;
-            for(int[] current : subTarget) {
-                field[current[0]][current[1]] -= 1;
+    public static void init() {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(field[i][j] == null) continue;
+                if(field[i][j].power <= 0) field[i][j] = null;
             }
         }
+
+        isVisited = new boolean[N][M];
+        queue = new LinkedList<Tower>();
+        damagedTower = new LinkedList<Tower>();
     }
 
-    static void findLaserWay(int r, int c, int[] target, boolean[][] isVisited, LinkedList<int[]> tempWay, int depth) {
-        if(r == target[0] && c == target[1]) {
-            subTarget = (LinkedList)tempWay.clone();
-            subTarget.remove(subTarget.size()-1);
-            return;
+    public static void destroyOrHeal() {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(field[i][j] == null) continue;
+                if(field[i][j].power <= 0) field[i][j] = null;
+                else field[i][j].power += 1;
+            }
+        }
+        attacker.power -= 1;
+        target.power -= 1;
+        for(Tower tower : damagedTower) {
+            tower.power -= 1;
         }
 
-        if(subTarget.size() != 0 && depth  >= subTarget.size()) return;
+        if(target.power <= 0) target = null;
+    }
 
-        for(int i=0; i<4; i++) {
+    public static void bombAttack() {
+        int r = target.r;
+        int c = target.c;
+        for(int i=0; i<8; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
             if(nr < 0) nr = N-1;
-            if(nr == N) nr = 0;
+            if(nr >= N) nr = 0;
             if(nc < 0) nc = M-1;
-            if(nc == M) nc = 0;
+            if(nc >= M) nc = 0;
 
-            if(isVisited[nr][nc] || field[nr][nc] <= 0) continue;
+            if(field[nr][nc] == null || (attacker.r == nr && attacker.c == nc)) continue;
 
-            isVisited[nr][nc] = true;
-            tempWay.add(new int[]{nr, nc});
-            findLaserWay(nr, nc, target, isVisited, tempWay, depth+1);
-            isVisited[nr][nc] = false;
-            tempWay.remove(tempWay.size()-1);
+            field[nr][nc].power -= (attacker.power/2);
+            damagedTower.offer(field[nr][nc]);
+            
         }
-
-        
     }
 
-    static int[] findAttacker(int k) {
-        int power = Integer.MAX_VALUE;
-        int r=0, c=0;
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(field[i][j] <= 0) continue;
+    public static void laserAttack() {
+        int r = target.backR;
+        int c = target.backC;
 
-                if(field[i][j] < power) {
-                    r = i;
-                    c = j;
-                    power = field[i][j];
-                    continue;
-                }
-                else if(field[i][j] == power) { // 공격력이 같다면
-                    if(attackTime[i][j] > attackTime[r][c]) {    // 가장 최근에 공격한 포탑이 약한 포탑
-                        r = i;
-                        c = j;
-                        power = field[i][j];
-                        continue;
-                    }
-                    else if(attackTime[i][j] == attackTime[r][c]) { // 공격 시간도 같다면
-                        if(r+c < i+j) { // 행과 열의 합이 큰게 약한 포탑
-                            r = i;
-                            c = j;
-                            power = field[i][j];
-                            continue;
-                        }
-                        else if(r+c == i+j) {   // 행과 열이 합이 같다면
-                            if(r < j) { //열의 값이 큰게 약한 포바
-                                r = i;
-                                c = j;
-                                power = field[i][j];
-                                continue;
-                            }
-                        }
-                    }
-                }
+        while(true) {
+            
+            Tower tower = field[r][c];
+
+            if(tower == attacker) break;
+
+            r = tower.backR;
+            c = tower.backC;
+
+            tower.power -= (attacker.power/2);
+            damagedTower.offer(tower);
+            
+        }
+    }
+
+    public static boolean laserPossible() {
+        boolean result = false;
+
+        while(!queue.isEmpty()) {
+            Tower currentTower = queue.poll();
+            int r = currentTower.r;
+            int c = currentTower.c;
+
+            if(currentTower == target) {
+                result = true;
+                break;
             }
-        }
 
-        attackTime[r][c] = k+1;
-        return new int[] {r, c};
-    }
+            for(int i=0; i<4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr < 0) nr = N-1;
+                if(nr >= N) nr = 0;
+                if(nc < 0) nc = M-1;
+                if(nc >= M) nc = 0;
 
-    static int[] findTarget(int y, int x) {
-        int power = Integer.MIN_VALUE;
-        int r=0, c=0;
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(field[i][j] <= 0 || (y==i && x==j)) continue;
+                if(field[nr][nc] == null || isVisited[nr][nc]) continue;
 
-                if(field[i][j] > power) {   // 공격력이 가장 큰게 강한 포탑
-                    r = i;
-                    c = j;
-                    power = field[i][j];
-                    continue;
-                }
-                else if(field[i][j] == power) { // 공격력이 같다면
-                    if(attackTime[i][j] < attackTime[r][c]) {    // 공격한지 가장 오래된 포탑이 강한 포탑
-                        r = i;
-                        c = j;
-                        power = field[i][j];
-                        continue;
-                    }
-                    else if(attackTime[i][j] == attackTime[r][c]) { // 공격 시간도 같다면
-                        if(r+c > i+j) { // 행과 열의 합이 작은게 강한 포탑
-                            r = i;
-                            c = j;
-                            power = field[i][j];
-                            continue;
-                        }
-                        else if(r+c == i+j) {   // 행과 열이 합이 같다면
-                            if(r > j) { //열의 값이 작은게 약한 포탑
-                                r = i;
-                                c = j;
-                                power = field[i][j];
-                                continue;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return power == Integer.MIN_VALUE ? new int[]{N, M} : new int[]{r, c};
-    }
-
-    static int findTower() {
-        int result = Integer.MIN_VALUE;
-
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(field[i][j] > result) result = field[i][j];
+                isVisited[nr][nc] = true;
+                Tower nextTower = field[nr][nc];
+                nextTower.backR = r;
+                nextTower.backC = c;
+                queue.offer(nextTower);
             }
         }
 
         return result;
+    }
+
+    public static void selectTower(int k) {
+        target = new Tower(Integer.MAX_VALUE, Integer.MAX_VALUE, 0);    // 제일 약한 경우를 넣음
+        attacker = new Tower(0, 0, Integer.MAX_VALUE);                              // 제일 강한 경우를 넣음
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(field[i][j] == null) continue;
+                if(field[i][j].compareTo(attacker) < 0 ) attacker = field[i][j];
+                if(field[i][j].compareTo(target) > 0) target = field[i][j];
+                field[i][j].backR = 0;
+                field[i][j].backC = 0;
+            }
+        }
+        attacker.power += N+M;
+        attacker.turn = k;
+    }
+
+    public static boolean isFinish(){
+        int unbrokenCnt = 0;
+        for(int i=0; i<N ;i++){
+            for(int j=0; j<M; j++){
+                if(field[i][j] == null) continue;
+                if(field[i][j].power > 0) unbrokenCnt++;
+            }
+        }
+        return unbrokenCnt <= 1 ? true : false;
+    }
+
+    public static int findStrongestTower(){
+        int max = 0;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(field[i][j] == null) continue;
+                max = Math.max(max,field[i][j].power);
+            }
+        }
+        return max;
     }
 }
