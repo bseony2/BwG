d64426e8402b3196d773b089cef3cf14211aba9e
bseony2 <bseony2@gmail.com>
2023-08-19 11:26:16 +0900

    코드트리 : 술래잡기 체스 - 시뮬레이션 (다시 푼 문제)

--- 배선영/술래잡기체스.java
+++ 배선영/술래잡기체스.java
@@ -2,14 +2,22 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.lang.Comparable;
 
-public class 술래잡기체스{
+public class 술래잡기체스 {
+
     static class Thief implements Comparable<Thief>{
-        int d, score;
-        public Thief(int score, int d) {
+        int r, c, d, score;
+        boolean isCaught;
+
+        public Thief(int r, int c, int d, int score, boolean isCaught) {
+            this.r = r;
+            this.c = c;
             this.d = d;
             this.score = score;
+            this.isCaught = isCaught;
         }
 
         public int compareTo(Thief thief) {
@@ -17,166 +25,112 @@ public class 술래잡기체스{
         }
     }
 
-    static class Police {
-        int r, c;
-        int d;
-
-        public Police(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
-
-    static Thief[][] map = new Thief[4][4];
+    static ArrayList<Thief> thiefList = new ArrayList<>();
+    static Thief[] initThieves = new Thief[17];
+    static int[][] initIdxMap = new int[4][4];
     static int ans = 0;
-    static Police police = new Police(0, 0);
     static int[] dr = new int[]{-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dc = new int[]{0, -1, -1, -1, 0, 1, 1, 1};
+    public static void main(String...args) throws IOException{
+        init();
 
-    public static void main(String[] args) throws IOException{
-        initialize();
-
-        play(ans);
+        backtracking(thiefList, initIdxMap, 0, 0, 0);
 
         System.out.println(ans);
     }
 
-    static void initialize() throws IOException{
+    static void init() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for(int i=0; i<4; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for(int j=0; j<4; j++) {
-                int score = Integer.parseInt(st.nextToken());
+                int idx = Integer.parseInt(st.nextToken());
                 int d = Integer.parseInt(st.nextToken())-1;
-                Thief thief = new Thief(score, d);
-                map[i][j] = thief;
+                Thief thief = new Thief(i, j, d, idx,false);
+                //initThieves[idx] = thief;
+                thiefList.add(thief);
+                initIdxMap[i][j] = idx-1;
             }
         }
-
-        ans += catchThief(0, 0);
-        moveThieves();
     }
 
-    static int catchThief(int r, int c) {
-        int score = map[r][c].score;
-        police.d = map[r][c].d;
-        police.r = r;
-        police.c = c;
-        map[r][c] = null;
-        return score;
-    }
+    static void backtracking(ArrayList<Thief> originThiefArr, int[][] originIdxMap, int r, int c, int score) {
+        ArrayList<Thief> thieves = thiefArrCopy(originThiefArr);
+        int[][] idxMap = idxMapCopy(originIdxMap);
 
-    static void play(int score) {
+        int thiefIdx = idxMap[r][c];
+        thieves.get(thiefIdx).isCaught = true;
+        int policeDirection = thieves.get(thiefIdx).d;
+        idxMap[r][c] = -1;
 
-        if(!isPoliceMoveable()) {
-            ans = ans > score ? ans : score;
-            return;
+        score += thieves.get(thiefIdx).score;
+        ans = ans > score ? ans : score;
+
+        thievesMove(thieves, idxMap, r, c);
+
+        for(int i=1; i<4; i++) {
+            int nr = r + dr[policeDirection]*i;
+            int nc = c + dc[policeDirection]*i;
+
+            if(!isValidPoint(nr, nc) || idxMap[nr][nc] == -1) continue;
+
+            backtracking(thieves, idxMap, nr, nc, score);
         }
-
-        Thief[][] tempMap = copyMap();
         
-        int r = police.r;
-        int c = police.c;
-        int d = police.d;
-        for(int dist=1; dist<=4; dist++) {
-            int nr = r + dr[d]*dist;
-            int nc = c + dc[d]*dist;
-
-            if(!isValidPoint(nr, nc) || map[nr][nc] == null) {
-                continue;
-            }
-
-            int catchScore = catchThief(nr, nc);
-            
-            moveThieves();
-
-            play(score + catchScore);
-            
-            police.r = r;
-            police.c = c;
-            police.d = d;
-            for(int i = 0; i < 4; i++)
-                for(int j = 0; j < 4; j++)
-                    map[i][j] = tempMap[i][j];
-        }
     }
 
-    static Thief[][] copyMap() {
-        Thief[][] result = new Thief[4][4];
-
-        for(int i=0; i<4; i++) {
-            for(int j=0; j<4; j++) {
-                if(map[i][j] == null) continue;
-                result[i][j] = map[i][j];
-            }
+    static ArrayList<Thief> thiefArrCopy(ArrayList<Thief> originList) {
+        ArrayList<Thief>result = new ArrayList<>();
+        for(Thief tempThief : originList) {
+            result.add(new Thief(tempThief.r, tempThief.c, tempThief.d, tempThief.score, tempThief.isCaught));
         }
-
+        Collections.sort(result);
         return result;
     }
 
-    static boolean isPoliceMoveable() {
-        int r = police.r;
-        int c = police.c;
+    static int[][] idxMapCopy(int[][] originArr) {
+        int[][] result = new int[4][4];
+        for(int i=0; i<4; i++) {
+            result[i] = originArr[i].clone();
+        }
+        return result;
+    }
 
-        while(true) {
-            int nr = r + dr[police.d];
-            int nc = c + dc[police.d];
+    static void thievesMove(ArrayList<Thief> thieves, int[][] idxMap, int policeR, int policeC) {
+        for(Thief thief : thieves) {
+            if(thief.isCaught) continue;
 
-            if(!isValidPoint(nr, nc)) {
-                return false;
+            int cr = thief.r;
+            int cc = thief.c;
+            int cd = thief.d;
+
+            for(int j=0; j<8; j++) {
+                int d = (cd + j) % 8;
+                int nr = cr + dr[d];
+                int nc = cc + dc[d];
+                if(!isValidPoint(nr, nc) || (nr == policeR && nc == policeC)) continue;
+
+                int nextThiefIdx = idxMap[nr][nc];
+
+                idxMap[nr][nc] = thief.score-1;
+                idxMap[cr][cc] = nextThiefIdx;
+
+                thief.r = nr;
+                thief.c = nc;
+                thief.d = d;
+
+                if(nextThiefIdx != -1) {
+                    Thief nextThief = thieves.get(nextThiefIdx);
+                    nextThief.r = cr;
+                    nextThief.c = cc;
+                }
+                
+                break;
             }
-            else if(map[nr][nc] != null) {
-                return true;
-            }
-            r = nr;
-            c = nc;
         }
     }
 
     static boolean isValidPoint(int r, int c) {
         return 0<=r && r<4 && 0<=c && c<4;
-    }
-
-    static void moveThieves() {
-        for(int k=1; k<=16; k++) {
-            moveThief(k);
-            
-        }
-    }
-
-    static void moveThief(int k) {
-        for(int r=0; r<4; r++) {
-            for(int c=0; c<4; c++) {
-                if(map[r][c] == null) continue;
-                if(map[r][c].score == k) {
-                    int d = map[r][c].d;
-                    for(int i=0; i<8; i++) {
-                        int nr = r + dr[d];
-                        int nc = c + dc[d];
-
-                        if(isThiefMoveAble(nr, nc)) {
-                            Thief newThief = new Thief(k, d);
-
-                            Thief temp = map[nr][nc];
-                            map[r][c] = temp;
-
-                            map[nr][nc] = newThief;
-                            break;
-                        }
-
-                        d = (d+1)%8;
-                    }
-                    return;
-                }
-            }
-        }
-    }
-
-    static boolean isThiefMoveAble(int r, int c) {
-        return isValidPoint(r, c) && (r != police.r || c != police.c);
-    }
-
-    static boolean isThiefLocated(int r, int c) {
-        return map[r][c] != null;
     }
 }
